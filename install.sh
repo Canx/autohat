c9319cd932d991d60e5cc09405d7cb04be96ff23
Ruben Cancho <canchete@gmail.com>
2017-11-12 10:28:09 +0100

    Added basic install script.

---
+++ install.sh
@@ -0,0 +1,29 @@
+# 1. Install chrome
+
+wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | sudo apt-key add - 
+sudo sh -c 'echo "deb [arch=amd64] http://dl.google.com/linux/chrome/deb/ stable main" >> /etc/apt/sources.list.d/google.list'
+sudo apt-get update
+sudo apt-get install google-chrome-stable
+
+# 1. Install chrome driver
+
+# platform options: linux32, linux64, mac64, win32
+PLATFORM=linux64
+VERSION=$(curl http://chromedriver.storage.googleapis.com/LATEST_RELEASE)
+curl http://chromedriver.storage.googleapis.com/$VERSION/chromedriver_$PLATFORM.zip \
+| bsdtar -xvf - -C env/bin/
+
+# 2. Download selenium
+
+wget http://selenium-release.storage.googleapis.com/3.7/selenium-server-standalone-3.7.1.jar
+
+# 3. Download composer.php
+
+php -r "copy('https://getcomposer.org/installer', 'composer-setup.php');"
+php -r "if (hash_file('SHA384', 'composer-setup.php') === '544e09ee996cdf60ece3804abc52599c22b1f40f4323403c44d44fdfdd586475ca9813a858088ffbc1f233e9b180f061') { echo 'Installer verified'; } else { echo 'Installer corrupt'; unlink('composer-setup.php'); } echo PHP_EOL;"
+php composer-setup.php
+php -r "unlink('composer-setup.php');"
+
+# 4. Install dependencies
+
+php composer.phar install
