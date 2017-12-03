a9db5def5f92a199b960fc2bcce830c4c9681988
Ruben Cancho <canchete@gmail.com>
2017-12-03 19:52:42 +0100

    Changed php version in install script.

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 # 0. Install php
 sudo apt-get update
-sudo apt-get -y install php7.0-cli php7.0-curl php7.0-zip php7.0-mbstring
+sudo apt-get -y install php7.1-cli php7.1-curl php7.1-zip php7.1-mbstring
 
 # 1. Install chrome
 
